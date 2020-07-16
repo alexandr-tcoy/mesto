9538513b47bb805a1ad502bedc939fd7c5ca2a74
Alexandr Tsoy <alexandr.tcoy@yandex.ru>
2020-07-16 23:29:09 +0300

    mesto_js

--- scripts/profileEdit.js
+++ scripts/profileEdit.js
@@ -1,27 +1,31 @@
-const modal = document.querySelector(".modal");
-const closeModalButton = document.querySelector(".modal__close-button");
-const form = modal.querySelector(".modal__form");
-const openModalButton = document.querySelector(".profile__edit");
-const title = document.querySelector(".profile__title");
-const subtitle = document.querySelector(".profile__subtitle");
-const modalName = document.querySelector(".modal__text_type_name");
-const modalAbout = document.querySelector(".modal__text_type_about");
-const saveModalButton = document.querySelector(".modal__button");
+const openModalButton = document.querySelector(".profile__edit"); 
+const title = document.querySelector(".profile__title"); 
+const subtitle = document.querySelector(".profile__subtitle"); 
+const modal = document.querySelector(".modal"); 
+const closeModalButton = document.querySelector(".modal__close-button"); 
+const modalName = document.querySelector(".modal__text_type_name"); 
+const modalAbout = document.querySelector(".modal__text_type_about"); 
+const form = modal.querySelector(".modal__form"); 
+const saveModalButton = document.querySelector(".modal__button"); 
 
-openModalButton.addEventListener ("click", toggleModal);
-closeModalButton.addEventListener ("click", toggleModal);
-saveModalButton.addEventListener ("click", toggleModal);
+function openModal() {
+    modal.classList.add("modal_is-open");
+    modalName.value = title.textContent;
+    modalAbout.value = subtitle.textContent;
+};
 
+function closeModal() {
+    modal.classList.remove("modal_is-open");
+};
 
-function toggleModal() {
-    modal.classList.toggle("modal_is-open");
-}
-
-function formSubmitHandler(evt) {
-    evt.preventDefault(); 
-    title.textContent = modalName.value;
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    modal.classList.remove("modal_is-open");
+    title.textContent = modalName.value; 
     subtitle.textContent = modalAbout.value;
 };
-form.addEventListener('submit', formSubmitHandler);
+form.addEventListener("submit", formSubmitHandler);
 
-
+openModalButton.addEventListener("click", openModal); 
+closeModalButton.addEventListener("click", closeModal);
+saveModalButton.addEventListener("click", formSubmitHandler);
