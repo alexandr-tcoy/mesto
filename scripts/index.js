7d4d70c145db213e55edc67a8297e0ea511f4256
Alexandr Tsoy <alexandr.tcoy@yandex.ru>
2020-10-04 00:00:04 +0300

    newVisionAndLastChance

--- scripts/index.js
+++ scripts/index.js
@@ -1,207 +1,153 @@
-const initialCards = [ 
-    { 
-        name: "Байкал", 
-        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg" 
-    }, 
-    { 
-        name: "Холмогорский район", 
-        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg" 
-    }, 
-    { 
-        name: "Камчатка", 
-        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg" 
-    }, 
-    { 
-        name: "Иваново", 
-        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg" 
-    }, 
-    { 
-        name: "Челябинская область", 
-        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg" 
-    }, 
-    { 
-        name: "Архыз", 
-        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg" 
-    } 
-]; 
- 
-const templateCard = document.querySelector(".template-card").content.querySelector(".element"); 
-const cardList = document.querySelector(".elements"); 
-const profile = document.querySelector(".profile"); 
-const profileButtonAdd = profile.querySelector(".profile__button"); 
-const profileButtonEdit = profile.querySelector(".profile__edit"); 
-const profileTitle = profile.querySelector(".profile__title"); 
-const profileSubtitle = profile.querySelector(".profile__subtitle"); 
-const modalTypeEdit = document.querySelector(".modal_type_edit"); 
-const modalEditButtonClose = modalTypeEdit.querySelector(".modal__close-button"); 
-const modalContainerEdit = modalTypeEdit.querySelector(".modal__container"); 
-const modalName = modalTypeEdit.querySelector(".modal__text_type_name"); 
-const modalAbout = modalTypeEdit.querySelector(".modal__text_type_about"); 
-const modalTypeAdd = document.querySelector(".modal_type_add"); 
-const modalAddButtonClose = modalTypeAdd.querySelector(".modal__close-button"); 
-const modalContainerAdd = modalTypeAdd.querySelector(".modal__container"); 
-const modalPlace = modalTypeAdd.querySelector(".modal__text_type_place"); 
-const modalUrl= modalTypeAdd.querySelector(".modal__text_type_url"); 
-const modalTypeImg = document.querySelector(".modal_type_img"); 
-const modalImgButtonClose = modalTypeImg.querySelector(".modal__close-button"); 
-const modalImg = modalTypeImg.querySelector(".modal__img"); 
-const modalUnderTitle = modalTypeImg.querySelector(".modal__under-title"); 
-const modalButton = modalTypeAdd.querySelector(".modal__button"); 
-const modalSet = { 
-    containerSelector: ".modal__container",  
-    inputListSelector: ".modal__text",  
-    buttonSelector: ".modal__button",  
-    disabledButtonSelector: "modal__button_disabled",  
-    classError: "modal__error_visible",  
-    inputError: "modal__text_type_error" 
-}; 
- 
-const escapeHandler = (evt, modal) => { 
-    if (evt.key === "Escape" && modal.classList.contains("modal_is-open")) { 
-        toggle(modal); 
-    }; 
-}; 
+const initialCards = [  
+    {  
+        name: "Байкал",  
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg"  
+    },  
+    {  
+        name: "Холмогорский район",  
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg"  
+    },  
+    {  
+        name: "Камчатка",  
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg"  
+    },  
+    {  
+        name: "Иваново",  
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg"  
+    },  
+    {  
+        name: "Челябинская область",  
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg"  
+    },  
+    {  
+        name: "Архыз",  
+        link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg"  
+    }  
+];  
 
-function toggle(modal) { 
-    modal.classList.toggle("modal_is-open"); 
-    toggleEscape(modal); 
-    toggleOverlay(modal); 
-    modalName.value = profileTitle.textContent; 
-    modalAbout.value = profileSubtitle.textContent; 
-}; 
- 
-function modalInputs (modal) { 
-    if (modal !== modalTypeImg) { 
-        modalContainerReset(modal, modalSet); 
-    }; 
-}; 
- 
-function editFormSubmitHandler(evt) { 
-    evt.preventDefault(); 
-    profileTitle.textContent = modalName.value; 
-    profileSubtitle.textContent = modalAbout.value;   
-    modalTypeEditInputs(modalTypeEdit); 
-}; 
- 
-function modalTypeEditInputs (modalTypeEdit) { 
-    if(modalTypeEdit.classList.contains("modal_is-open")){ 
-    }; 
-    toggle(modalTypeEdit); 
-}; 
- 
-function addFormSubmitHandler(evt) { 
-    evt.preventDefault(); 
-    renderCard({name: modalPlace.value, link: modalUrl.value}); 
-    toggle(modalTypeAdd); 
-}; 
- 
-const clickImg = (evt) => { 
-    toggle(modalTypeImg); 
-    console.log(evt.target.src); 
-    modalImg.src = evt.target.src; 
-    modalUnderTitle.textContent = evt.target.closest(".element").querySelector(".element__text").textContent; 
-}; 
- 
-const toggleLike = (evt) => { 
-    evt.target.classList.toggle("element__like_active"); 
-}; 
- 
-const deleteClosest = (evt) => { 
-    evt.target.closest(".element").remove(); 
-}; 
- 
-initialCards.forEach((data) => { 
-    renderCard(data); 
-}); 
- 
-function renderCard(data) { 
-    cardList.prepend(joinCard(data)); 
-}; 
- 
-function joinCard(data) { 
-    const elementCard = templateCard.cloneNode(true); 
-    const elementPhoto = elementCard.querySelector(".element__photo"); 
-    const elementText = elementCard.querySelector(".element__text"); 
-    const elementLikeButton = elementCard.querySelector(".element__like"); 
-    const elementDeleteButton = elementCard.querySelector(".element__delete"); 
-    elementPhoto.src = data.link; 
-    elementText.textContent = data.name; 
-    elementPhoto.alt = data.name; 
-    elementLikeButton.addEventListener("click", toggleLike); 
-    elementDeleteButton.addEventListener("click", deleteClosest); 
-    elementPhoto.addEventListener("click", clickImg); 
-    return elementCard; 
-}; 
- 
-profileButtonEdit.addEventListener("click", () => { 
-    toggle(modalTypeEdit); 
-}); 
- 
-modalEditButtonClose.addEventListener("click", () => { 
-    toggle(modalTypeEdit); 
-}); 
- 
-modalContainerEdit.addEventListener("submit",editFormSubmitHandler); 
- 
-profileButtonAdd.addEventListener("click", () => { 
-    toggle(modalTypeAdd); 
-    modalPlace.value = ""; 
-    modalUrl.value =""; 
-}); 
- 
-modalAddButtonClose.addEventListener("click", () => { 
-    toggle(modalTypeAdd); 
-}); 
- 
-modalContainerAdd.addEventListener("submit", addFormSubmitHandler); 
- 
-modalImgButtonClose.addEventListener("click", () => { 
-    toggle(modalTypeImg); 
-}); 
- 
-const modalIsOpen = (modal) => { 
-    return modal.classList.contains("modal_is-open"); 
-}; 
- 
-const evtModalIsOpen = (evt) => { 
-    return evt.target.classList.contains("modal_is-open") 
-}; 
- 
-const eventListenersAddEscape = (modal) => { 
-    document.addEventListener("keydown", (evt) => escapeHandler(evt, modal));     
-}; 
- 
-const eventListenersRemoveEscape = function (modal) { 
-    document.removeEventListener("keydown", (evt) => escapeHandler(evt, modal)); 
-}; 
+const content = document.querySelector(".content");
+const profile = content.querySelector(".profile");
+const editProfileModal = document.querySelector(".modal_type_edit");
+const openEditProfileModal = profile.querySelector(".profile__edit");
+const closeEditProfileModal = document.querySelector(".modal__close-button");
+const saveEditProfileModal = document.querySelector(".modal__button");
+const formEditModal = editProfileModal.querySelector(".modal__form");
+const profileName = profile.querySelector(".profile__title");
+const profileAbout = profile.querySelector(".profile__subtitle");
+const inputName = formEditModal.querySelector(".modal__text_type_name");
+const inputAbout = formEditModal.querySelector(".modal__text_type_about");
+const addPlaceModal = document.querySelector(".modal_type_add");
+const openAddPlaceModal = document.querySelector(".profile__button");
+const closeAddPlaceModal = addPlaceModal.querySelector(".modal__close-button");
+const saveAddPlaceModal = addPlaceModal.querySelector(".modal__button");
+const formAddModal = addPlaceModal.querySelector(".modal__form");
+const inputPlace = addPlaceModal.querySelector(".modal__text_type_place");
+const inputUrl = addPlaceModal.querySelector(".modal__text_type_url");
+const imageModal = document.querySelector(".modal_type_img");
+const titleImageModal = document.querySelector(".modal__under-title");
+const closeImageModal = imageModal.querySelector(".modal__close-button");
+const templateCard = document.querySelector(".template-card").content;
+const elements = content.querySelector(".elements");
 
-const overlayHandler = (evt, modal) => { 
-        if (evtModalIsOpen(evt)) { 
-            toggle(modal); 
-    }; 
-}; 
+function toggleModal(modal) {
+    const opened = modal.classList.contains("modal_is-open");
+    if (opened) {
+        document.removeEventListener("keydown", closeEcape)
+        document.removeEventListener("click", closeOverlay);
+    } else {
+        document.addEventListener("keydown", closeEcape)
+        document.addEventListener("click", closeOverlay);
+    }
+    modal.classList.toggle("modal_is-open");
+};
 
-const eventListenersAddOverlay = (modal) => { 
-    modal.addEventListener("mouseup", (evt) => overlayHandler(evt, modal));     
-}; 
- 
-const eventListenersRemoveOverlay = (modal) => { 
-    modal.removeEventListener("mouseup", (evt) => overlayHandler(evt, modal)); 
-}; 
- 
-const  toggleOverlay = (modal) => { 
-    if (modalIsOpen(modal)) { 
-        eventListenersAddOverlay(modal); 
-    } else { 
-        eventListenersRemoveOverlay(modal); 
-    }; 
-}; 
- 
-const  toggleEscape = (modal) => { 
-    if (modalIsOpen(modal)) { 
-        eventListenersAddEscape(modal); 
-    } else { 
-        eventListenersRemoveEscape(modal); 
-        modalInputs (modal); 
-    }; 
-}; 
+function toggleEditProfileModal(editProfileModal) {
+    toggleModal(editProfileModal);
+    if (editProfileModal.classList.contains("modal_is-open")) {
+        inputName.value = profileName.textContent;
+        inputAbout.value = profileAbout.textContent;
+    };
+
+    saveEditProfileModal.classList.remove("modal__button_disabled");
+    saveEditProfileModal.removeAttribute("disabled");
+    hideInputError(editProfileModal, inputName, testKit);
+    hideInputError(editProfileModal, inputAbout, testKit);
+};
+
+function toggleAddProfileModal(addPlaceModal) {
+    hideInputError(addPlaceModal, inputUrl, testKit);
+    hideInputError(addPlaceModal, inputPlace, testKit);
+    document.getElementsByName("add").disabled = true;
+
+    toggleModal(addPlaceModal);
+    inputPlace.value = "";
+    inputUrl.value = "";
+};
+
+function closeOverlay(evt) {
+    if (evt.target.classList.contains("modal")) {
+        toggleModal(document.querySelector(".modal_is-open"));
+    }
+};
+
+function closeEcape(evt) {
+    if (evt.key === "Escape") {
+        toggleModal(document.querySelector(".modal_is-open"));
+    }
+};
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileAbout.textContent = inputAbout.value;
+    toggleEditProfileModal(editProfileModal);
+};
+
+function openImage(evt) {
+    const clickedImage = evt.target;
+    toggleModal(imageModal);
+    imageModal.querySelector(".modal__img").src = clickedImage.src;
+    titleImageModal.textContent = evt.target.closest(".element").querySelector(".element__text").textContent; 
+};
+
+function generateCard(initialCards) {
+    const card = templateCard.cloneNode(true);
+    const cardImage = card.querySelector(".element__photo");
+    cardImage.src = initialCards.link;
+    card.querySelector(".element__text").textContent = initialCards.name;
+    card.querySelector(".element__photo").addEventListener("click", openImage);
+    card.querySelector(".element__like").addEventListener("click", (evt) => {
+        evt.target.classList.toggle("element__like_active");
+    });
+    card.querySelector(".element__delete").addEventListener("click", (evt) => {
+        evt.target.closest(".element").remove();
+    });
+    saveAddPlaceModal.classList.add("modal__button_disabled");
+    saveAddPlaceModal.setAttribute("disabled", true);
+    return card;
+};
+
+function additionNewCard(card) {
+    const addition = generateCard(card);
+    elements.prepend(addition);
+};
+
+initialCards.forEach(card => {
+    elements.append(generateCard(card))
+});
+
+function formSubmitHandlerCard(evt) {
+    evt.preventDefault();
+    const newPlace = {name: inputPlace.value, link: inputUrl.value};
+    additionNewCard(newPlace);
+    toggleAddProfileModal(addPlaceModal);
+    document.getElementsByName("add").disabled = true;
+};
+
+openEditProfileModal.addEventListener("click", () => toggleEditProfileModal(editProfileModal));
+closeEditProfileModal.addEventListener("click", () => toggleEditProfileModal(editProfileModal));
+editProfileModal.addEventListener("submit", formSubmitHandler);
+openAddPlaceModal.addEventListener("click", () => toggleAddProfileModal(addPlaceModal));
+closeAddPlaceModal.addEventListener("click", () => toggleAddProfileModal(addPlaceModal));
+addPlaceModal.addEventListener("submit", formSubmitHandlerCard);
+closeImageModal.addEventListener("click", () => toggleModal(imageModal));
--- scripts/validate.js
+++ scripts/validate.js
@@ -1,77 +1,72 @@
-const showInputError = (formElement, inputElement, errorMessage, {classError, inputError}) => {
-    const errorElement =  formElement.querySelector(`#${inputElement.id}-error`); 
-    inputElement.classList.add(inputError);
-    errorElement.classList.add(classError);
+const testKit = {
+    formSelector: ".modal__form",
+    inputSelector: ".modal__text",
+    submitButtonSelector: ".modal__button",
+    inactiveButtonClass: "modal__button_disabled",
+    inputErrorClass: "modal__text_type_error",
+    errorClass: "modal__error_visible"
+};
+
+const showInputError = (formElement, inputElement, errorMessage, testKit) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.add(testKit.inputErrorClass);
     errorElement.textContent = errorMessage;
+    errorElement.classList.add(testKit.errorClass);
 };
 
-const hideInputError = (formElement, inputElement, {classError, inputError}) => {
-    const errorElement =  formElement.querySelector(`#${inputElement.id}-error`); 
-    inputElement.classList.remove(inputError);
-    errorElement.classList.remove(classError);
-    errorElement.TextContent = "";
-};
-
-const checkInputValidity = (formElement, inputElement, {...remain}) => {
-    if (!inputElement.validity.valid) {
-        showInputError(formElement, inputElement, inputElement.validationMessage, remain); 
-    } else {
-        hideInputError(formElement, inputElement, remain);
-    };
+const hideInputError = (formElement, inputElement, testKit) => {
+    const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+    inputElement.classList.remove(testKit.inputErrorClass);
+    errorElement.classList.remove(testKit.errorClass);
+    errorElement.textContent = "";
 };
 
 const hasInvalidInput = (inputList) => {
-    return inputList.some((inputElement) => !inputElement.validity.valid); 
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    })
 };
 
-const modalButtonAktive = (modalButtons, {disabledButtonSelector, ...remain}) => {
-    modalButtons.classList.remove(disabledButtonSelector);
-    modalButtons.disabled = false;
-};
-
-const modalButtonDisabled = (modalButtons, {disabledButtonSelector, ...remain}) => {
-    modalButtons.classList.add(disabledButtonSelector);
-    modalButtons.disabled = true;
-};
-
-const toggleButtonState = (inputList, modalButtons, {...remain}) => {
-    if (hasInvalidInput(inputList)) {
-        modalButtonDisabled(modalButtons, remain);
+const checkInputValidity = (formElement, inputElement, testKit) => {
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage, testKit);
     } else {
-        modalButtonAktive(modalButtons, remain);
-    };
+        hideInputError(formElement, inputElement, testKit);
+    }
 };
 
-const setEventListeners = (formElement,{inputListSelector, buttonSelector, ...remain}) => {
-    const inputList = Array.from(formElement.querySelectorAll(inputListSelector));
-    const modalButtons = formElement.querySelector(buttonSelector);
-    toggleButtonState(inputList, modalButtons, remain);
+const toggleButtonState = (inputList, buttonElement, testKit) => {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.classList.add(testKit.inactiveButtonClass);
+        buttonElement.disabled = true;
+    } else {
+        buttonElement.classList.remove(testKit.inactiveButtonClass);
+        buttonElement.disabled = false;
+    }
+};
+
+const setEventListeners = (formElement, testKit) => {
+    const inputList = Array.from(formElement.querySelectorAll(testKit.inputSelector));
+    const buttonElement = formElement.querySelector(testKit.submitButtonSelector);
+
+    toggleButtonState(inputList, buttonElement, testKit);
+
     inputList.forEach((inputElement) => {
-        inputElement.addEventListener("input", (evt) => {
-        checkInputValidity(formElement, inputElement, remain);
-        toggleButtonState(inputList, modalButtons, remain);
+        inputElement.addEventListener("input", function () {
+            checkInputValidity(formElement, inputElement, testKit);
+            toggleButtonState(inputList, buttonElement, testKit);
         });
     });
 };
 
-const enableValidation = ({containerSelector, ...remain}) => {
-    const formList = Array.from(document.querySelectorAll(containerSelector));
+const enableValidation = (testKit) => {
+    const formList = Array.from(document.querySelectorAll(testKit.formSelector));
     formList.forEach((formElement) => {
         formElement.addEventListener("submit", (evt) => {
-            evt.preventDefault();    
+            evt.preventDefault();
         });
-    setEventListeners(formElement, remain);
-});
+        setEventListeners(formElement, testKit);
+    });
 };
 
-enableValidation(modalSet);
-
-function  modalContainerReset(modal, {containerSelector, inputListSelector, buttonSelector, ...rest}){ 
-    const specificModalContainer =  modal.querySelector(containerSelector); 
-    const specificInputListFormList = Array.from(specificModalContainer.querySelectorAll(inputListSelector)); 
-    const specificButton = specificModalContainer.querySelector(buttonSelector); 
-    specificInputListFormList.forEach((specificInputListFormListElement) => { 
-        hideInputError(specificModalContainer, specificInputListFormListElement, rest); 
-        toggleButtonState(specificInputListFormList, specificButton, rest); 
-        }); 
-    }; 
+enableValidation(testKit);
