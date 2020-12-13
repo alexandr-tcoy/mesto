8ff1871d1cffdc4c6e75ad8419eb908ea0eb6c61
Alexandr Tsoy <alexandr.tcoy@yandex.ru>
2020-12-14 02:06:47 +0300

    pleaseGodTwo

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,9 @@
 export class Modal {
-    constructor(modalSelector) {
-        this._element = document.querySelector(modalSelector);
-    }
+   
+constructor(modalSelector) { 
+    this._element = document.querySelector(modalSelector); 
+    this._handleEscClose = this._handleEscClose.bind(this);
+}
 
     _handleEscClose(evt) {
         if (evt.key === 'Escape') {
@@ -9,15 +11,15 @@ export class Modal {
         }
     }
 
-    open() {
-        this._element.classList.add('modal_is-open');
-        document.addEventListener('keydown', (evt) => this._handleEscClose(evt));
-    }
+open() {
+    this._element.classList.add('modal_is-open');
+    document.addEventListener('keydown', this._handleEscClose);
+}
 
-    close() {
-        this._element.classList.remove('modal_is-open');
-        document.removeEventListener('keydown', (evt) => this._handleEscClose(evt));
-    }
+close() {
+    this._element.classList.remove('modal_is-open');
+    document.addEventListener('keydown', this._handleEscClose);
+}
 
     setEventListeners() {
         this._element.querySelector('.modal__close-button').addEventListener('click', () => {
@@ -31,3 +33,86 @@ export class Modal {
         })
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export class Modal {
+//     constructor(modalSelector) {
+//         this._element = document.querySelector(modalSelector);
+//     }
+
+    
+//     _handleEscClose(evt) {
+//         if (evt.key === 'Escape') {
+//             this.close()
+//         }
+//     }
+
+//     open() {
+//         this._element.classList.add('modal_is-open');
+//         document.addEventListener('keydown', (evt) => this._handleEscClose(evt));
+//     }
+
+//     close() {
+//         this._element.classList.remove('modal_is-open');
+//         document.removeEventListener('keydown', (evt) => this._handleEscClose(evt));
+//     }
+
+//     setEventListeners() {
+//         this._element.querySelector('.modal__close-button').addEventListener('click', () => {
+//             this.close();
+//         })
+//         this._element.addEventListener('mousedown', (evt) => {
+//             if (evt.target !== evt.currentTarget) {
+//                 return
+//             }
+//             this.close();
+//         })
+//     }
+// }
+
